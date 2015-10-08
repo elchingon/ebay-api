b31703d514d4758605bbf4878d052d6a7471dfa6
Alexi Carey <ciegoproductions@gmail.com>
2015-10-08 15:05:30 -0600

    Add new nodes to ProductListingDetails type

--- lib/ebay/types/product_listing_details.rb
+++ lib/ebay/types/product_listing_details.rb
@@ -18,6 +18,13 @@ module Ebay # :nodoc:
       boolean_node :use_stock_photo_url_as_gallery, 'UseStockPhotoURLAsGallery', 'true', 'false', :optional => true
       text_node :stock_photo_url, 'StockPhotoURL', :optional => true
       text_node :copyright, 'Copyright', :optional => true
+      text_node :upc, 'UPC', :optional => true
+      text_node :ean, 'EAN', :optional => true
+      text_node :isbn, 'ISBN', :optional => true
+      text_node :product_reference_id, 'ProductReferenceID', :optional => true
+      boolean_node :include_ebay_product_details, 'IncludeeBayProductDetails', 'true', 'false', :optional => true
+      boolean_node :return_search_result_on_duplicates, 'ReturnSearchResultOnDuplicates', 'true', 'false', :optional => true
+      boolean_node :use_first_product, 'UseFirstProduct', 'true', 'false', :optional => true
     end
   end
 end
